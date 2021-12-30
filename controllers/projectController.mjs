1a017ae5ebcb50d0939f5dafb45a0df6ea338133
EmmyMay <lekeemmy@gmail.com>
2021-12-30 01:56:33 +0100

    Get Searched Project by title

--- controllers/projectController.mjs
+++ controllers/projectController.mjs
@@ -1,4 +1,4 @@
-import   Mongoose from 'mongoose';
+import Mongoose from 'mongoose';
 import Project from '../database/model/Project.mjs'
 import Student from '../database/model/Student.mjs'
 
@@ -6,7 +6,18 @@ import Student from '../database/model/Student.mjs'
 
 
 export const saveProjectMetaData = async (req, res) => {
-  if (!req?.body?.user.firstName || !req?.body?.project.title) {
+  console.log(req.body);
+  const checkForDuplicate = await Project.findOne({
+    title: req.body.title
+  });
+  if (checkForDuplicate) {
+    res.status(409).json({
+      message: "That Project already exists"
+    })
+    return;
+  }
+  console.log(checkForDuplicate);
+  if (!req ? .body ? .firstName || !req ? .body ? .title) {
     return res.status(400).json({
       'message': 'User and Project Data required'
     });
@@ -21,6 +32,7 @@ export const saveProjectMetaData = async (req, res) => {
       phone,
       option
     } = req.body;
+
     const student = await Student.create({
       _id: new Mongoose.Types.ObjectId(),
       firstName,
@@ -28,7 +40,8 @@ export const saveProjectMetaData = async (req, res) => {
       middleName,
       email,
       phone,
-      option
+      option,
+      project_id: new Mongoose.Types.ObjectId()
     })
 
     const {
@@ -38,14 +51,47 @@ export const saveProjectMetaData = async (req, res) => {
       tags
     } = req.body;
     const project = await Project.create({
+      _id: student.project_id,
       author: student._id,
       title,
       submittedOn,
       supervisedBy,
       tags
     })
+    res.status(201).json({
+      message: "Project Successfully Saved"
+    })
 
   } catch (error) {
     console.error(error);
+    res.status(400).json({
+      message: "unable to save project"
+    })
   }
+}
+
+export const getProject = async (req, res) => {
+
+  // Search By Project title
+  try {
+    var phrase = "\"" + req.body.title + "\"";
+    const project = await Project.find({
+      $text: {
+        $search: phrase
+      }
+    })
+    console.log(project);
+
+    res.status(200).json({
+      message: "Found Project!",
+      data: project
+    })
+
+
+  } catch (error) {
+    res.status(400).json({
+      message: "Project not found!"
+    })
+  }
+
 }
